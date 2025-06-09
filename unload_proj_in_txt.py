278126ec2f1b89f684f9fbe87aa4c8911c821edd
Mikhail Akmurzin <117809088+Mis-prog@users.noreply.github.com>
2025-06-09 05:34:55 +0000

    Переделал сборщик кода

--- unload_proj_in_txt.py
+++ unload_proj_in_txt.py
@@ -1,15 +1,17 @@
 import os
 
-folder_path = 'source'
+folder_paths = ['source', 'example']  
 output_file = 'all_code.txt'
 
 with open(output_file, 'w', encoding='utf-8') as out:
-    for root, _, files in os.walk(folder_path):
-        for filename in sorted(files):
-            if filename.endswith(('.py', '.cpp', '.h', '.c', '.java', '.js')):  # нужные расширения
-                filepath = os.path.join(root, filename)
-                out.write(f"# ==== {filename} ====\n")
-                with open(filepath, 'r', encoding='utf-8') as f:
-                    for line in f:
-                        if line.strip():  # удаляет пустые строки
-                            out.write(line)
+    for folder_path in folder_paths:
+        for root, _, files in os.walk(folder_path):
+            for filename in sorted(files):
+                if filename.endswith(('.py', '.cpp', '.h', '.c', '.java', '.js')):
+                    filepath = os.path.join(root, filename)
+                    relative_path = os.path.relpath(filepath, start=folder_path)
+                    out.write(f"# ==== {os.path.join(folder_path, relative_path)} ====\n")
+                    with open(filepath, 'r', encoding='utf-8') as f:
+                        for line in f:
+                            if line.strip():
+                                out.write(line)
