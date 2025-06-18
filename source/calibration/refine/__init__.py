d8c7b66f4cbb0a708a3f168bafda9c54d8c5bd20
Михаил Акмурзин <akmurzinmihail@gmail.com>
2025-06-18 18:43:51 +0500

    Тесты

--- source/calibration/refine/__init__.py
+++ source/calibration/refine/__init__.py
@@ -1,7 +1,7 @@
 from .optimizer import RefineOptimizer
 from .back_error_funk import residual_interline_distance, residual_parallel_group, residual_line_length, \
     residual_planar_alignment, residual_vertical_alignment, residual_alignment_block
-from .direct_error_funk import residual_reprojection_line
+from .direct_error_funk import residual_reprojection_line, residual_reprojection_point
 from .refine_angle import get_plane_normal
 
 __all__ = [
@@ -13,5 +13,8 @@ __all__ = [
     "residual_planar_alignment",
     "residual_vertical_alignment",
     "residual_alignment_block",
-    "get_plane_normal"
+    "get_plane_normal",
+    "residual_reprojection_point"
 ]
+
+
--- source/calibration/refine/direct_error_funk.py
+++ source/calibration/refine/direct_error_funk.py
@@ -3,25 +3,52 @@ import numpy as np
 from source.core import Camera, PointND
 from source.calibration.utils import gps_to_enu
 
-RESUALDS = []
 
-
-def residual_reprojection_line(camera, data, group, gps_origin):
+def residual_reprojection_line(camera, data, group, gps_origin=None):
     residuals = []
     lines = data.get(group, [])
     for line in lines:
-        p1, p2 = line['pixel']
-        P1, P2 = line['gps']
-        _p1 = camera.project_direct(PointND([*gps_to_enu(*P1, *gps_origin), 0])).get()
-        _p2 = camera.project_direct(PointND([*gps_to_enu(*P2, *gps_origin), 0])).get()
-        error1 = np.sum((np.array(_p2) - np.array(p2)) ** 2)
-        error2 = np.sum((np.array(_p1) - np.array(p1)) ** 2)
-        error = np.sqrt(error1 + error2)
-        # print(f"DEBUG error: {error}, error1: {np.sqrt(error1)}, error2: {np.sqrt(error2)}")
-        RESUALDS.append([error, error1, error2])
-        residuals.append(error)
-        # residuals.append(error1)
-        # residuals.append(error2)
+        if gps_origin is not None:
+            p1, p2 = line['pixel']
+            P1, P2 = line['gps']
+            _p1 = camera.project_direct(PointND([*gps_to_enu(*P1, *gps_origin), 0])).get()
+            _p2 = camera.project_direct(PointND([*gps_to_enu(*P2, *gps_origin), 0])).get()
+            error1 = np.sum((np.array(_p2) - np.array(p2)) ** 2)
+            error2 = np.sum((np.array(_p1) - np.array(p1)) ** 2)
+            error = np.sqrt(error1 + error2)
+            residuals.append(error)
+        else:
+            p1, p2 = line['pixel']
+            P1, P2 = line['gps']
+            _p1 = camera.project_direct(PointND([*P1])).get()
+            _p2 = camera.project_direct(PointND([*P2])).get()
+            error1 = np.sum((np.array(_p2) - np.array(p2)) ** 2)
+            error2 = np.sum((np.array(_p1) - np.array(p1)) ** 2)
+            error = np.sqrt(error1 + error2)
+            residuals.append(error)
+
+    return residuals
+
+
+def residual_reprojection_point(camera, data, group, gps_origin=None):
+    residuals = []
+    points = data.get(group, [])
+    for point in points:
+        if gps_origin is not None:
+            p1 = point['pixel']
+            P1 = point['gps']
+            _p1 = camera.project_direct(PointND([*gps_to_enu(*P1, *gps_origin), 0])).get()
+            error2 = np.sum((np.array(_p1) - np.array(p1)) ** 2)
+            error = np.sqrt(error2)
+            residuals.append(error)
+        else:
+            p1 = point['pixel']
+            P1 = point['gps']
+            # print(p1, P1)
+            _p1 = camera.project_direct(PointND([*P1])).get()
+            error2 = np.sum((np.array(_p1) - np.array(p1)) ** 2)
+            error = np.sqrt(error2)
+            residuals.append(error)
     return residuals
 
 
